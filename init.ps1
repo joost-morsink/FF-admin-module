a8860025cf5664574be973d9bb68d33f49c514b7
Joost W. Morsink <morsink@gmail.com>
2021-09-26 20:53:23 +0200

    Rearrange web solution

--- init.ps1
+++ init.ps1
@@ -1,10 +1,18 @@
 #!/usr/local/bin/pwsh
 
-if(!(Test-Path ./events/.git)){
-    $username = Read-Host -Prompt "Git user name? "
-    $password = Read-Host -Prompt "Git password? " -MaskInput
+Push-Location $PSScriptRoot
+try {
+    if(!(Test-Path ./events/.git)){
+        $username = Read-Host -Prompt "Git user name? "
+        $password = Read-Host -Prompt "Git password? " -MaskInput
 
-    git clone "https://$($username):$($password)@gitlab.com/future-fund/event-data.git" ./events
+        git clone "https://$($username):$($password)@gitlab.com/future-fund/event-data.git" ./events
+    }
+
+    Push-Location src/web
+    docker build .
+    docker compose up -d
+    Pop-Location
+} finally {
+    Pop-Location
 }
-
-docker compose up -d
