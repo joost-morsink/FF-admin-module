cb9795cd0e61798f9a51b29ba627f86cd08b73a6
Joost W. Morsink <morsink@gmail.com>
2021-10-26 05:40:46 +0200

    Window closing prompt on open eventstore session

--- src/web/FfAdminWeb/ClientApp/src/app/app.component.ts
+++ src/web/FfAdminWeb/ClientApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EventStore } from './backend/eventstore';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(private eventStore: EventStore) {
+    window.onbeforeunload = e => this.closing(e);
+  }
+  private closing(e: any) {
+    if (this.eventStore.wasSessionAvailable()) {
+      e.preventDefault();
+      e.returnValue = '';
+    } else {
+      delete e['returnValue'];
+    }
+  }
 }
--- src/web/FfAdminWeb/ClientApp/src/app/backend/eventstore.ts
+++ src/web/FfAdminWeb/ClientApp/src/app/backend/eventstore.ts
@@ -7,19 +7,24 @@ export class EventStore {
   constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
 
   }
-
-  public isSessionAvailable(): Promise<boolean> {
-    return this.http.get<boolean>(this.baseUrl + "eventstore/session/is-available").toPromise();
+  private lastSessionAvailable: boolean;
+  public wasSessionAvailable(): boolean {
+    return this.lastSessionAvailable;
+  }
+  public async isSessionAvailable(): Promise<boolean> {
+    return this.lastSessionAvailable = await this.http.get<boolean>(this.baseUrl + "eventstore/session/is-available").toPromise();
   }
 
   public async startSession(): Promise<boolean> {
     await this.http.post(this.baseUrl + "eventstore/session/start", {}).toPromise();
+    this.lastSessionAvailable = true;
     return true;
   }
 
   public async endSession(message: string): Promise<boolean> {
     console.log(`ending session with ${message}`);
     await this.http.post(this.baseUrl + "eventstore/session/stop", { message: message }).toPromise();
+    this.lastSessionAvailable = false;
     return true;
   }
   public async postEvent(ev: IEvent) {
