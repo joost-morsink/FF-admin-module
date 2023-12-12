b45758728ca720783ad6de69e1843fe6c9995182
Joost W. Morsink <joost@giveforgood.world>
2023-12-12 06:56:53 +0100

    Fix GiveWP import timestamps

--- src/web/External.GiveWp.ApiClient/DateTimeOffsetFormat.cs
+++ src/web/External.GiveWp.ApiClient/DateTimeOffsetFormat.cs
@@ -7,13 +7,13 @@ namespace External.GiveWp.ApiClient;
 
 internal class DateTimeOffsetFormat : JsonConverter<DateTimeOffset>
 {
-    private static TimeZoneInfo _cetTimeZone = TimeZoneInfo.FindSystemTimeZoneById("CET");
+    private static readonly DateTime SWITCH_TO_UTC = new DateTime(2023, 12, 05, 15, 37,0);
     public override DateTimeOffset Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         if (reader.TokenType == JsonTokenType.String)
         {
-            if (DateTime.TryParseExact(reader.GetString(), "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None,out var date))
-                return new DateTimeOffset(date, _cetTimeZone.GetUtcOffset(date));
+            if (DateTime.TryParseExact(reader.GetString(), "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out var date))
+                return new DateTimeOffset(date,  date < SWITCH_TO_UTC ? TimeSpan.FromHours(2) : TimeSpan.Zero);
         }
         throw new JsonException();
     }
