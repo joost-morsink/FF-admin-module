d6bcb6a114d33c04605c75240cae76fe4c35d6f8
Joost W. Morsink <joost@giveforgood.world>
2024-12-03 10:34:37 +0100

    Transfers to csv

---
+++ scripts/transfers.ps1
@@ -0,0 +1,19 @@
+param (
+    $Branch = "Main"
+)
+$charities = (iwr https://g4g-calculator.azurewebsites.net/api/$Branch/charities).Content | ConvertFrom-Json
+$att = (iwr https://g4g-calculator.azurewebsites.net/api/$Branch/amounts-to-transfer).Content | ConvertFrom-Json
+$lines = New-Object -TypeName System.Collections.ArrayList
+foreach($key in ($att | Get-Member -Type NoteProperty))
+{
+    $id = $key.Name
+    $charity = $charities.$id
+    foreach($entry in ($att.$id.Amounts | Get-Member -Type NoteProperty)){
+        $curr = $entry.Name
+        $amount = $att.$id.Amounts.$curr
+        $line = @{Id=$id;Name=$charity.Name;BankName=$charity.Bank.Name;BankBic=$charity.Bank.Bic;BankAccount=$charity.Bank.Account;Currency=$curr;Amount=$amount}
+        [void] $lines.Add($line)
+    }
+}
+
+$lines | Select-object -Property Id,Name,BankName,BankBic,BankAccount,Currency,Amount | ConvertTo-Csv
